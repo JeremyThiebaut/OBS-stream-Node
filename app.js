913a01c9ccbdeb41e2f977a5526db8cc681b123a
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-01-30 15:44:46 +0100

    save

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-var NodeMediaServer = require('node-media-server');
+const NodeMediaServer = require('node-media-server');
 
-var config = {
+const config = {
   rtmp: {
     port: 1935,
     chunk_size: 60000,
     gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
+    ping: 30,
+    ping_timeout: 60
   },
   http: {
     port: 8000,
-    mediaroot: './media',
     allow_origin: '*'
-  },
-  trans: {
-    ffmpeg: '/usr/local/bin/ffmpeg',
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
   }
 };
 
